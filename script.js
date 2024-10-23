503ac19db552caaf0e8c0da9caa3b3ab85345da8
Hotel-Finder <hotel.finder0@gmail.com>
2024-10-23 08:19:12 -0700

    thx beyonce

--- script.js
+++ script.js
@@ -11,46 +11,47 @@ class Obj {
         windowObjs.push(this)
     }
 
-    V(){
-        return Math.sqrt(this.v[0]*this.v[0]+this.v[1]*this.v[1]);
+    V() {
+        return Math.sqrt(this.v[0] * this.v[0] + this.v[1] * this.v[1]);
     }
-    P(){
-        return this.mass*this.V();
+    P() {
+        return this.mass * this.V();
     }
-    KE(){
-        return this.mass*this.V()*this.V()/2;
+    KE() {
+        return this.mass * this.V() * this.V() / 2;
     }
-    
+
 }
-class Square extends Obj{
-    constructor(mass, pos, vi,width) {
+class Square extends Obj {
+    constructor(mass, pos, vi, width) {
         super();
         this.mass = mass;
         this.pos = pos;
         this.v = vi;
         this.width = width;
-        this.shape="square";
+        this.shape = "square";
         windowObjs.push(this)
     }
-    collision(other, dir){
+    collision(other, dir) {
         //x stuff:
-        /*
-        const vafx = ((this.mass-other.mass)*this.v[0]+2*other.mass*other.v[0])/(this.mass+other.mass);
-        const vbfx = ((other.mass-this.mass)*other.v[0]+2*this.mass*this.v[0])/(this.mass+other.mass);
+
+        const vafx = ((this.mass - other.mass) * this.v[0] + 2 * other.mass * other.v[0]) / (this.mass + other.mass);
+        const vbfx = ((other.mass - this.mass) * other.v[0] + 2 * this.mass * this.v[0]) / (this.mass + other.mass);
         this.v[0] = vafx;
         other.v[0] = vbfx;
-        const vafy = ((this.mass-other.mass)*this.v[1]+2*other.mass*other.v[1])/(this.mass+other.mass);
-        const vbfy = ((other.mass-this.mass)*other.v[1]+2*this.mass*this.v[1])/(this.mass+other.mass);
+        const vafy = ((this.mass - other.mass) * this.v[1] + 2 * other.mass * other.v[1]) / (this.mass + other.mass);
+        const vbfy = ((other.mass - this.mass) * other.v[1] + 2 * this.mass * this.v[1]) / (this.mass + other.mass);
         this.v[1] = vafy;
         other.v[1] = vbfy;
-        */
-       //console.log({dir})
-       let vy = this.v[1]+other.v[1]
-       let vx = this.v[0] + other.v[0]
-       
+
+        //console.log({dir})
+        let dy = this.v[1] + other.v[1]
+        let dx = this.v[0] + other.v[0]
+
+
     }
 }
-function toDegrees(){
+function toDegrees() {
 
 }
 function init() {
@@ -61,7 +62,7 @@ let lastTime = 0;
 
 function loop(timeStamp) {
     const ctx = document.getElementById("canvas").getContext("2d");
-    
+
 
     // Calculate deltaTime in seconds (convert from milliseconds)
     let deltaTime = (timeStamp - lastTime) / 1000;
@@ -71,18 +72,18 @@ function loop(timeStamp) {
     ctx.clearRect(0, 0, 400, 400);
 
     windowObjs.forEach(i => {
-        if(i.shape=="square"){
+        if (i.shape == "square") {
             ctx.beginPath();
             ctx.rect(i.pos[0], i.pos[1], i.width, i.width);
             ctx.stroke();
 
             ctx.strokeStyle = "red"
             ctx.beginPath();
-            ctx.arc(i.pos[0]+i.width/2,i.pos[1]+i.width/2,1,0,2*Math.PI);
+            ctx.arc(i.pos[0] + i.width / 2, i.pos[1] + i.width / 2, 1, 0, 2 * Math.PI);
             ctx.stroke();
             ctx.beginPath();
-            ctx.moveTo(i.pos[0]+i.width/2,i.pos[1]+i.width/2);
-            ctx.lineTo(i.pos[0]+i.v[0]+i.width/2,i.pos[1]+i.v[1]+i.width/2);
+            ctx.moveTo(i.pos[0] + i.width / 2, i.pos[1] + i.width / 2);
+            ctx.lineTo(i.pos[0] + i.v[0] + i.width / 2, i.pos[1] + i.v[1] + i.width / 2);
             ctx.stroke();
             ctx.strokeStyle = "black";
         }
@@ -95,16 +96,16 @@ function loop(timeStamp) {
         i.pos[1] += i.v[1] * deltaTime;
 
         //check collision:
-        windowObjs.forEach(j=>{
-            
-            if(i != j){
+        windowObjs.forEach(j => {
+
+            if (i != j) {
                 //right side
-                if(i.pos[0] + 100 - j.pos[0] > 0 && j.pos[0] > i.pos[0] && !i.collidedWith.includes(j) && Math.abs(i.pos[1]-j.pos[1]) < 100){
+                if (i.pos[0] + 100 - j.pos[0] > 0 && j.pos[0] > i.pos[0] && !i.collidedWith.includes(j) && Math.abs(i.pos[1] - j.pos[1]) < 100) {
                     /* dir_i = Math.atan2(i.v[1],i.v[0])+Math.PI;
                     dir_j = Math.atan2(j.v[1],j.v[0])+Math.PI;
                     console.log(2*(dir_i+dir_j)); */
                     //console.log("COLLIDE")
-                    i.collision(j,[i.v[0]+j.v[0],i.v[1]+j.v[1]])
+                    i.collision(j, [i.v[0] + j.v[0], i.v[1] + j.v[1]])
                     i.collidedWith.push(j);
                     j.collidedWith.push(i);
                 }
@@ -118,6 +119,6 @@ function loop(timeStamp) {
 // Start the loop
 
 const windowObjs = []
-const sqr1 = new Square(1,[50,50],[10,0],100)
-const sqr2 = new Square(1,[250,50],[-10,0], 100)
+const sqr1 = new Square(1, [50, 50], [10, 0], 100)
+const sqr2 = new Square(1, [250, 50], [-10, 1], 100)
 init();
